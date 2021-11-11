448e66a0061034e6c6dfa7952f08b67484977165
GuadalupeCa <guadalupe.carranzavilla@everis.nttdata.com>
2021-11-11 01:42:28 -0500

    initial push

---
+++ src/main/java/com/finance/debitTransactionms/DebitTransactionmsApplication.java
@@ -0,0 +1,13 @@
+package com.finance.debitTransactionms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitTransactionmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebitTransactionmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finance/debitTransactionms/DebitTransactionmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.debitTransactionms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitTransactionmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
